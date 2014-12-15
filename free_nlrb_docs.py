e71f55cb666119bdec9f7f6613dad796879a4291
Matt Johnson <mjohn23@gmail.com>
2014-12-15 13:14:42 -0600

    Get document URL

--- free_nlrb_docs.py
+++ free_nlrb_docs.py
@@ -1,6 +1,8 @@
 import feedparser
 
+print "Grabbing NLRB Decisions Feed..."
 board_decisions_feed = feedparser.parse('http://www.nlrb.gov/rss/rssBoardDecisions.xml')
+print "\nParsing Entries..."
 board_decisions_entries = board_decisions_feed['entries']
-
+print "\nFirst Document URL:"
 print board_decisions_entries[0]['links'][0]['href']
