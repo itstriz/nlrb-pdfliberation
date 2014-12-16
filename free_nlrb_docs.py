145020d46a19f110403f8c5ca1ef530ba79ab5a7
Matt Johnson <mjohn23@gmail.com>
2014-12-16 11:52:30 -0600

    Add framework for template checking

--- free_nlrb_docs.py
+++ free_nlrb_docs.py
@@ -10,20 +10,34 @@ def get_pdf_file(url, output_file):
     temp_pdf_file.write(html)
     temp_pdf_file.close()
 
+def extract_from_template(data_file):
+    """ Extract from template """
+    # Open file
+    nlrb_doc = open(data_file, 'r')
+    first_line = nlrb_doc.readline()
+
+    # Template 1
+    if first_line[0:6] == 'UNITED':
+        return "Template found for" + data_file
+    else:
+        return "No Template Found for " + data_file
+    
 print "Grabbing NLRB Decisions Feed..."
 board_decisions_feed = feedparser.parse('http://www.nlrb.gov/rss/rssBoardDecisions.xml')
 
 print "\nParsing Entries..."
 board_decisions_entries = board_decisions_feed['entries']
 
-print "\nFirst Document URL:"
-board_decision_doc = board_decisions_entries[0]['links'][0]['href']
-print board_decision_doc
-
 foo = 1
 for entry in board_decisions_entries:
-    get_pdf_file(entry['links'][0]['href'], 'sample' + str(foo) + '.pdf')
-    os.system('pdf2txt.py sample' + str(foo) + '.pdf > sample' + str(foo) + '.txt')
+    doc_url = entry['links'][0]['href']
+    file_name = 'sample' + str(foo) + '.pdf'
+    txt_name = 'sample' + str(foo) + '.txt'
+    print "Getting " + doc_url
+    get_pdf_file(doc_url, file_name)
+    print "Converting " + file_name + " to text"
+    os.system('pdf2txt.py ' + file_name + ' > ' + txt_name)
+    print "Checking for template: " + extract_from_template(txt_name)
     foo = foo + 1
 
 
